f06247dda93140d1255928537fa2efcd5cbb6181
André <139287902+pxdroAndre@users.noreply.github.com>
2024-10-16 15:43:47 -0300

    colocando valores de retorno
    só pra facilitar no figma

--- simulacao.jl
+++ simulacao.jl
@@ -2,15 +2,19 @@ using Printf
 
 function validar_cliente(renda::Float64, parcela::Float64)
     if parcela > (renda * 0.3)
+        return 1
         error("A parcela deve ser menor ou igual a 30% da renda do cliente\n")
     end
+    return 0
 end 
  
 function simular_financiamento(prazo::Int, valor_inicial::Float64, taxa_juros::Float64, valor_entrada::Float64, renda_cliente::Float64)
     if prazo < 1
+        return 1
         error("O prazo deve ser positivo e maior ou igual a 1\n")
     end
-    if taxa_juros < 0 
+    if taxa_juros < 0
+        return 1
         error("A taxa de juros não pode ser negativa\n")
     end
 
@@ -21,9 +25,11 @@ function simular_financiamento(prazo::Int, valor_inicial::Float64, taxa_juros::F
     valor_total = parcela_mensal * prazo
     juros_total = valor_total - valor_bruto
     
-    validar_cliente(renda_cliente, parcela_mensal)
-    
-    @printf("O valor total será de %.2f reais\n", valor_total)
-    @printf("O valor pago mensalmente será de %.2f\n", parcela_mensal)
-    @printf("O valor do juros pago é de %.2f\n", juros_total)
+    if (validacao=validar_cliente(renda_cliente, parcela_mensal) == 1) 
+        return 1
+    end
+    return valor_total, parcela_mensal, juros_total
+    # @printf("O valor total será de %.2f reais\n", valor_total)
+    # @printf("O valor pago mensalmente será de %.2f\n", parcela_mensal)
+    # @printf("O valor do juros pago é de %.2f\n", juros_total)
 end
