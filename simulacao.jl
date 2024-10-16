d9d7817e7b74b144da6999a13657c42dfbd36c69
André <139287902+pxdroAndre@users.noreply.github.com>
2024-10-16 16:05:59 -0300

    Update simulacao.jl
    fiz merda e arrumei

--- simulacao.jl
+++ simulacao.jl
@@ -2,20 +2,20 @@ using Printf
 
 function validar_cliente(renda::Float64, parcela::Float64)
     if parcela > (renda * 0.3)
-        return 1
         error("A parcela deve ser menor ou igual a 30% da renda do cliente\n")
+        return 1
     end
     return 0
 end 
  
 function simular_financiamento(prazo::Int, valor_inicial::Float64, taxa_juros::Float64, valor_entrada::Float64, renda_cliente::Float64)
     if prazo < 1
-        return 1
         error("O prazo deve ser positivo e maior ou igual a 1\n")
+        return 1
     end
     if taxa_juros < 0
-        return 1
         error("A taxa de juros não pode ser negativa\n")
+        return 1
     end
 
     #Calculo do juros e parcela mensal de acordo com a formula usada pelo banco central
@@ -28,8 +28,8 @@ function simular_financiamento(prazo::Int, valor_inicial::Float64, taxa_juros::F
     if (validacao=validar_cliente(renda_cliente, parcela_mensal) == 1) 
         return 1
     end
-    return valor_total, parcela_mensal, juros_total
     # @printf("O valor total será de %.2f reais\n", valor_total)
     # @printf("O valor pago mensalmente será de %.2f\n", parcela_mensal)
     # @printf("O valor do juros pago é de %.2f\n", juros_total)
+    return valor_total, parcela_mensal, juros_total
 end
