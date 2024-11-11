4cdb9355690d1cd8ad5de7f9821a7d0b12348ee8
André <139287902+pxdroAndre@users.noreply.github.com>
2024-11-11 14:50:08 -0300

    Update api.jl

--- api.jl
+++ api.jl
@@ -1,10 +1,14 @@
 using HTTP
 include("acompanhar.jl")
 include("renegociar.jl")
+include("requisitar.jl")
 
 function handle_request(req::HTTP.Request)
     println("Metodo: ", req.method)
     println("Target: ", req.target)
+    body = String(req.body)
+    println("Corpo da Requisição: ", body)
+
     if req.method == "GET" && startswith(req.target, "/acompanhar")
         # Separar o caminho do endpoint dos parâmetros de query
         uri = HTTP.URIs.URI(req.target)
@@ -21,14 +25,36 @@ function handle_request(req::HTTP.Request)
             return HTTP.Response(400, "Parâmetro 'cpf' não encontrado")
         end
     elseif req.method == "POST" && req.target == "/renegociar"
-        # Parsear o corpo do request como JSON
-        body = String(req.body)
-        data = JSON.parse(body)
-        resultado = renegociar(data["cpf"], data["novos_juros"], data["novo_prazo"], data["nova_renda"])
-        if resultado == 1
-            return HTTP.Response(200, "Renegociação feita com sucesso!")
-        else
-            return HTTP.Response(400, "Renegociação negada!")
+        try
+            data = JSON.parse(body)
+            println("Dados Recebidos: ", data)
+
+            resultado = renegociar(data["cpf"], data["novos_juros"], data["novo_prazo"], data["nova_renda"])
+            println("Resultado da Renegociação: ", resultado)
+            if resultado isa String
+                return HTTP.Response(400, resultado)  # Retorna a mensagem de erro
+            else
+                return HTTP.Response(200, resultado)  # Retorna o JSON com os dados atualizados
+            end
+        catch e
+            println("Erro ao processar requisição: ", e)
+            return HTTP.Response(400, "Erro ao processar dados")
+        end
+    elseif req.method == "POST" && req.target == "/requisitar"
+        try
+            data = JSON.parse(body)
+            println("Dados Recebidos: ", data)
+
+            resultado = requisitar(data["prazo"], data["valor_solicitado"], data["valor_entrada"], data["cpf"], data["tipo_financiamento"])
+            println("Resultado da Requisição: ", resultado)
+            if resultado isa String
+                return HTTP.Response(400, resultado)  # Retorna a mensagem de erro
+            else
+                return HTTP.Response(200, resultado)  # Retorna o JSON com os dados do financiamento
+            end
+        catch e
+            println("Erro ao processar requisição: ", e)
+            return HTTP.Response(400, "Erro ao processar dados")
         end
     else
         return HTTP.Response(404, "Endpoint não encontrado")
